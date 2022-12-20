7a4e56f42078cc5959f1d26493020de280dec235
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-20 14:53:57 -0800

    Added Boolean Logic Assignment

---
+++ Basic_C#_Programs/BoolLogic/BoolLogic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BoolLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            Console.WriteLine("Have you ever had a DUI?");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            Console.WriteLine("Qualified?");
+            bool qualified = Convert.ToInt32(age) > 15 & !Convert.ToBoolean(dui) & Convert.ToInt32(tickets) <= 3;
+            Console.WriteLine(qualified);
+            Console.Read();
+        }
+    }
+}

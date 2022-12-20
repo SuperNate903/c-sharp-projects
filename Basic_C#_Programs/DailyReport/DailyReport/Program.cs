023a370d12723301bc15311ec8ed033c57b9e0ec
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-20 12:47:38 -0800

    Added variables for conversions

--- Basic_C#_Programs/DailyReport/DailyReport/Program.cs
+++ Basic_C#_Programs/DailyReport/DailyReport/Program.cs
@@ -14,17 +14,17 @@ namespace DailyReport
             string course = Console.ReadLine();
             Console.WriteLine("What page number?");
             string pagenum = Console.ReadLine();
-            Convert.ToInt32(pagenum);
+            int pageint = Convert.ToInt32(pagenum);
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");
             string help = Console.ReadLine();
-            Convert.ToBoolean(help);
+            bool helpbool = Convert.ToBoolean(help);
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
             string experience = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
             string hours = Console.ReadLine();
-            Convert.ToInt32(hours);
+            int inthours = Convert.ToInt32(hours);
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.Read();
         }

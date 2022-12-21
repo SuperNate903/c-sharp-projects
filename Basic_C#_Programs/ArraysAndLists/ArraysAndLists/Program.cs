2472ccf1e84634a8e69d18ef56a79c9a48c92fc2
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-21 14:32:46 -0800

    Fixed errors

--- Basic_C#_Programs/ArraysAndLists/ArraysAndLists/Program.cs
+++ Basic_C#_Programs/ArraysAndLists/ArraysAndLists/Program.cs
@@ -13,7 +13,7 @@ namespace ArraysAndLists
             {
                 Console.WriteLine("Choose a string:\n0. " + stringArray[0] + "\n1. " + stringArray[1] + "\n2. " + stringArray[2]);
                 index = Convert.ToInt32(Console.ReadLine());
-                if(index >= 0 & index <= 3)
+                if(index >= 0 & index <= 2)
                 {
                     Console.WriteLine("You chose the string: " + stringArray[index] + ".");
                 }
@@ -22,13 +22,13 @@ namespace ArraysAndLists
                     Console.WriteLine("That index does not exist.");
                 }
             }
-            while(index < 0 & index > 3);
+            while(index < 0 | index > 2);
             int[] integerArray = new int[] { 16, 12, 9 };
             do
             {
                 Console.WriteLine("Choose an integer:\n0. " + integerArray[0] + "\n1. " + integerArray[1] + "\n2. " + integerArray[2]);
                 index = Convert.ToInt32(Console.ReadLine());
-                if (index >= 0 & index <= 3)
+                if (index >= 0 & index <= 2)
                 {
                     Console.WriteLine("You chose the integer: " + integerArray[index] + ".");
                 }
@@ -37,7 +37,7 @@ namespace ArraysAndLists
                     Console.WriteLine("That index does not exist.");
                 }
             }
-            while (index < 0 & index > 3);
+            while (index < 0 | index > 2);
             List<string> stringList = new List<string>();
             stringList.Add("Apricot");
             stringList.Add("Peach");
@@ -46,7 +46,7 @@ namespace ArraysAndLists
             {
                 Console.WriteLine("Choose a string:\n0. " + stringList[0] + "\n1. " + stringList[1] + "\n2. " + stringArray[2]);
                 index = Convert.ToInt32(Console.ReadLine());
-                if (index >= 0 & index <= 3)
+                if (index >= 0 & index <= 2)
                 {
                     Console.WriteLine("You chose the string: " + stringList[index] + ".");
                 }
@@ -55,7 +55,7 @@ namespace ArraysAndLists
                     Console.WriteLine("That index does not exist.");
                 }
             }
-            while (index < 0 & index > 3);
+            while (index < 0 | index > 2);
             Console.Read();
         }
     }

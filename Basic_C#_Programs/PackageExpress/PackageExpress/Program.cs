040f9e32e069080b87aeb34fb04f78a1d9b04c47
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-21 11:28:56 -0800

    Added Branching Assignment

---
+++ Basic_C#_Programs/PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if(weight <= 50)
+            {
+                Console.WriteLine("Please enter the package width");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package height");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter the package length");
+                int length = Convert.ToInt32(Console.ReadLine());
+                if(width + height + length <= 50)
+                {
+                    int total = (width * height * length * weight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + total);
+                    Console.WriteLine("Thank you!");
+                    Console.Read();
+                }
+                else
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+                    Console.Read();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.Read();
+            }
+        }
+    }
+}

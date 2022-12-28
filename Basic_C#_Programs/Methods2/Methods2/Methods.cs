33dbdc5ad6e98452c68fb281f32824508afdc928
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-28 11:51:31 -0800

    Added second Method assignment

---
+++ Basic_C#_Programs/Methods2/Methods2/Methods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods2
+{
+    class Methods
+    {
+        public static int Operation(int number)
+        {
+            return number + 12;
+        }
+
+        public static decimal Operation(decimal decimalNum)
+        {
+            return decimalNum / 4;
+        }
+
+        public static int Operation(string stringNum)
+        {
+            return Convert.ToInt32(stringNum) * 3;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Methods2/Methods2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Methods.Operation(number));
+            Console.WriteLine("Input a decimal");
+            decimal decimalNum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(Methods.Operation(decimalNum));
+            Console.WriteLine("Input a number (in string format)");
+            string stringNum = Console.ReadLine();
+            Console.WriteLine(Methods.Operation(stringNum));
+            Console.Read();
+        }
+    }
+}

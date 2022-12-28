9446993394e5ed3217a73fd867f41307c7e7e862
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-27 19:18:21 -0800

    Added Methods Assignment

---
+++ Basic_C#_Programs/Methods/Methods/Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    public class Methods
+    {
+        public static int Method1(int number)
+        {
+            return number + 7;
+        }
+
+        public static int Method2(int number)
+        {
+            return number * 4;
+        }
+
+        public static int Method3(int number)
+        {
+            return number / 12;
+        }
+
+        //public int Method4(int number)
+        //{
+        //    return number / 12;
+        //}
+
+        //public void Method5(int number)
+        //{
+            
+        //}
+    }
+}
---
+++ Basic_C#_Programs/Methods/Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write in a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            // This calls the Method1 method in the Methods class
+            Console.WriteLine(Methods.Method1(input));
+            // This calls the Method2 method in the Methods class
+            Console.WriteLine(Methods.Method2(input));
+            // This calls the Method3 method in the Methods class
+            Console.WriteLine(Methods.Method3(input));
+            //Methods obj1 = new Methods();
+            //Console.WriteLine(obj1.Method4(input));
+            Console.ReadLine();
+        }
+    }
+}

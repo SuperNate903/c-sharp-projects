8fa33b4beed42c78858520af5431b5b1f13adc6d
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-28 18:43:34 -0800

    Added forth Method assignment

---
+++ Basic_C#_Programs/Methods4/Methods4/Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods4
+{
+    class Methods
+    {
+        public Methods()
+        {
+
+        }
+
+        public void Operation(int num1, int num2)
+        {
+            int total = num1 - num2;
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Basic_C#_Programs/Methods4/Methods4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Methods4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Methods method = new Methods();
+            Console.WriteLine("Write a number.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write another number.");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            method.Operation(number1, number2);
+        }
+    }
+}

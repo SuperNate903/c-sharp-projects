9a8973210be6052caeb84d8bbf1902d2c8bdd9af
SuperNate903 <minecraft.nate04@outlook.com>
2022-12-29 18:36:34 -0800

    Added Class Method assignment

---
+++ Basic_C#_Programs/ClassMethods/ClassMethods/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethods
+{
+    public static class Methods
+    {
+        public static void Divide(int num, out int total)
+        {
+            total = num / 2;
+        }
+
+        public static void Divide(string stringNum, out int total)
+        {
+            total = Convert.ToInt32(stringNum) / 5;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassMethods/ClassMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMethods
+{
+    class Program
+    {
+        static void Main()
+        {
+            int total = 0;
+            Console.WriteLine("Write a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Methods.Divide(number, out total);
+            Console.WriteLine(total);
+            Console.WriteLine("Write another number");
+            string stringNumber = Console.ReadLine();
+            Methods.Divide(stringNumber, out total);
+            Console.WriteLine(total);
+            Console.Read();
+        }
+    }
+}

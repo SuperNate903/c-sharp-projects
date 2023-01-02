3bd45a2ab04a74af4be9e273640608a5f4390df3
SuperNate903 <minecraft.nate04@outlook.com>
2023-01-02 14:35:40 -0800

    Added Operators Assignment

---
+++ Basic_C#_Programs/Operators/Operators/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators
+{
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+
+        public static Employee operator== (Employee obj1, Employee obj2)
+        {
+            if(obj1.FirstName == obj2.FirstName & obj1.LastName == obj2.LastName)
+            {
+                return obj1;
+            }
+            else
+            {
+                return obj2;
+            }
+        }
+        public static Employee operator!= (Employee obj1, Employee obj2)
+        {
+            if (obj1.FirstName != obj2.FirstName & obj1.LastName != obj2.LastName)
+            {
+                return obj1;
+            }
+            else
+            {
+                return obj2;
+            }
+        }
+    }
+}
---
+++ Basic_C#_Programs/Operators/Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee emp1 = new Employee() { FirstName = "Bob", LastName = "Smith" };
+            Employee emp2 = new Employee() { FirstName = "Bob", LastName = "Smith" }; // Susan
+
+            Console.WriteLine(emp1 == emp2);
+            Console.WriteLine(emp1 != emp2);
+            Console.ReadLine();
+        }
+    }
+}

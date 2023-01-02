727067d1ee098dc5b3998e491c57aebac809eaf2
SuperNate903 <minecraft.nate04@outlook.com>
2023-01-02 14:33:36 -0800

    Added Twenty One Game

---
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    public abstract class Game
+    {
+        public List<Player> Players { get; set; }
+        public string Name { get; set; }
+        public string Dealer { get; set; }
+
+        public abstract void Play();
+
+        public virtual void ListPlayers()
+        {
+            foreach(Player player in Players)
+            {
+                Console.WriteLine(player.Name);
+            }
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TwentyOne
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Jesse";
+            game += player;
+            game -= player;
+
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach(Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of" + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
     }
 }

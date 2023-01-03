a7b4073fb030c9567f727641b662cc09301d30df
SuperNate903 <minecraft.nate04@outlook.com>
2023-01-02 19:49:22 -0800

    Added Parsing Enum Assignment

---
+++ Basic_C#_Programs/ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("What is the current day of the week?");
+                Console.WriteLine("The day of the week is " + Enum.Parse(typeof(DotW), Console.ReadLine()) + ".");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.Read();
+        }
+    }
+    public enum DotW
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}

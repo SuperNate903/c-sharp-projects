118f27f7ea93f2d5691230edbacc1cd1a01aa2d8
SuperNate903 <minecraft.nate04@outlook.com>
2023-01-02 19:49:37 -0800

    Added more in the Twenty One project

--- Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
@@ -4,8 +4,32 @@ using System.Text;
 
 namespace TwentyOne
 {
-    class Card
+    public class Card
     {
-
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+    }
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     }
 }

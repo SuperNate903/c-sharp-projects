a01aaad680d18af4fe436fd1d38f87510a7b82a1
SuperNate903 <minecraft.nate04@outlook.com>
2023-01-05 13:03:32 -0800

    Added constructor assignment

---
+++ Basic_C#_Programs/Constructors/Constructors/ChainConstruct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    public class ChainConstruct
+    {
+        public ChainConstruct(int number) : this(number, 5)
+        {
+            
+        }
+
+        public ChainConstruct(int num1, int num2)
+        {
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Constructors/Constructors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int number = 27;
+            var stringVar = "This is a string";
+
+            Console.WriteLine(number);
+            Console.WriteLine(stringVar);
+
+            Console.WriteLine("We're going to add a number to 8. Write a number.");
+            ChainConstruct constructor = new ChainConstruct(Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}

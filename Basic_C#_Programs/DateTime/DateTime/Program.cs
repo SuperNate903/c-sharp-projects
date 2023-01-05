855f1f368d59dff237479d872fb6b8d668d4d9fe
SuperNate903 <minecraft.nate04@outlook.com>
2023-01-04 17:32:01 -0800

    Added DateTime assignment

---
+++ Basic_C#_Programs/DateTime/DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NDateTime
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Write a number that will determine what the current time will be in that amount of hours.");
+            Console.WriteLine(new DateTime
+            (
+                DateTime.Now.Year,
+                DateTime.Now.Month,
+                DateTime.Now.Day,
+                DateTime.Now.Hour + Convert.ToInt32(Console.ReadLine()),
+                DateTime.Now.Minute,
+                DateTime.Now.Second
+            ));
+            Console.Read();
+        }
+    }
+}
